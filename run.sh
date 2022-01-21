347e4ba04b22a9d760ea7c9955b55075ad030330
Lucas Klemmer <lucas.klemmer@jku.at>
2022-01-21 17:14:54 +0100

    readme

--- run.sh
+++ run.sh
@@ -3,30 +3,6 @@
 WORKSPACE=$(pwd)
 SERV=$WORKSPACE/fusesoc_libraries/serv
 
-# install fusesoc and SERV
-if [ ! -d "fusesoc_libraries" ] 
-then
-fusesoc library add fusesoc_cores https://github.com/fusesoc/fusesoc-cores
-fusesoc library add serv https://github.com/olofk/serv
-fi
-
-if ! command -v verilator &> /dev/null
-then
-    echo "verilator could not be found"
-    exit
-fi
-
-# clone compliance tests
-if [ ! -d "riscv-compliance" ] 
-then
-    git clone https://github.com/riscv/riscv-compliance --branch 1.0
-    # build servant core
-    fusesoc run --target=verilator_tb --build servant --vcd
-    # build and run compliance tests once, how can I activate vcd dumps here?
-    cd riscv-compliance && make TARGETDIR=$SERV/riscv-target RISCV_TARGET=serv RISCV_DEVICE=rv32i RISCV_ISA=rv32i RISCV_PREFIX=riscv32-unknown-elf- TARGET_SIM=$WORKSPACE/build/servant_1.1.0/verilator_tb-verilator/Vservant_sim
-    cd ..
-fi
-
 # run compliance tests again but dump vcd and rename it
 for i in $(find $WORKSPACE/riscv-compliance/work/rv32i/ -name '*.hex'); do
     vcd_target=eval/traces/$(basename -s .hex $i).vcd
@@ -37,5 +13,5 @@ for i in $(find $WORKSPACE/riscv-compliance/work/rv32i/ -name '*.hex'); do
     fi
 done
 
-cd eval && ./eval-compliance.sh && cd ..
+cd eval && pipenv run ./eval-compliance.sh && cd ..
 cat serv-cpi.txt

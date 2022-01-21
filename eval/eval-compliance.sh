8935c6ac9222c197a124ec58eaa6a0e3e84a5449
Lucas Klemmer <lucas.klemmer@jku.at>
2022-01-21 16:09:39 +0100

    pipfile and run.sh

--- eval/eval-compliance.sh
+++ eval/eval-compliance.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Start analyzing CPI"
+
 printf "SERV CPI on Compliance Tests\n\n" > ../serv-cpi.txt
 echo "    Opcode       Avg.        Min        Max" >> ../serv-cpi.txt
 
@@ -10,15 +12,18 @@ SLT SLTI SLTU SLTIU
 SLL SLLI SRA SRAI SRL SRLI"
 
 for op in $single_ops; do
-    wawk inst.wawk traces/I-$op-01.elf.vcd ${op,,} > ../serv-cpi.txt
+    printf "."
+    wawk inst.wawk traces/I-$op-01.elf.vcd ${op,,} >> ../serv-cpi.txt
 done
-
+exit
 branch_ops="JAL JALR BEQ BGE BGEU BLT BLTU BNE"
 for op in $branch_ops; do
+    printf "."    
     wawk inst.wawk traces/I-MISALIGN_JMP-01.elf.vcd ${op,,} >> ../serv-cpi.txt
 done
 
 memory_ops="LB LBU LH LHU LW SB SH SW"
 for op in $memory_ops; do
+    printf "."    
     wawk inst.wawk traces/I-MISALIGN_LDST-01.elf.vcd ${op,,} >> ../serv-cpi.txt
 done

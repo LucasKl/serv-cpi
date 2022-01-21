d784a7ab33e0969ae078ca66a1c19e40adbf1245
Lucas Klemmer <lucas.klemmer@jku.at>
2022-01-21 16:05:18 +0100

    update run.sh

--- eval/eval-compliance.sh
+++ eval/eval-compliance.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm -f res1 res2 res3
+printf "SERV CPI on Compliance Tests\n\n" > ../serv-cpi.txt
+echo "    Opcode       Avg.        Min        Max" >> ../serv-cpi.txt
 
 single_ops="AUIPC LUI 
 ADD ADDI SUB AND ANDI OR ORI XOR XORI
@@ -8,28 +9,16 @@ ECALL
 SLT SLTI SLTU SLTIU
 SLL SLLI SRA SRAI SRL SRLI"
 
-(for op in $single_ops; do
-    wawk inst.wawk traces/I-$op-01.elf.vcd ${op,,} >> res1
- done) &
-p1=$!
+for op in $single_ops; do
+    wawk inst.wawk traces/I-$op-01.elf.vcd ${op,,} > ../serv-cpi.txt
+done
 
 branch_ops="JAL JALR BEQ BGE BGEU BLT BLTU BNE"
-(for op in $branch_ops; do
-    wawk inst.wawk traces/I-MISALIGN_JMP-01.elf.vcd ${op,,} >> res2
- done) &
-p2=$!
+for op in $branch_ops; do
+    wawk inst.wawk traces/I-MISALIGN_JMP-01.elf.vcd ${op,,} >> ../serv-cpi.txt
+done
 
 memory_ops="LB LBU LH LHU LW SB SH SW"
-(for op in $memory_ops; do
-    wawk inst.wawk traces/I-MISALIGN_LDST-01.elf.vcd ${op,,} >> res3
- done) &
-p3=$!
-
-wait $p1
-wait $p2
-wait $p3
-
-printf "SERV CPI on Compliance Tests\n\n" > result
-echo "    Opcode       Avg.        Min        Max" >> result
-cat res1 res2 res3 >> ../serv-cpi.txt
-rm -f res1 res2 res3
+for op in $memory_ops; do
+    wawk inst.wawk traces/I-MISALIGN_LDST-01.elf.vcd ${op,,} >> ../serv-cpi.txt
+done
